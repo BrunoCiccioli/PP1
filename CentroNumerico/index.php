83d681cb9f274ae18c6440f65820f7c34e5b470a
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-09-03 20:32:38 -0300

    Add files via upload

---
+++ CentroNumerico/index.php
@@ -0,0 +1,52 @@
+<?php
+	
+	$sumaMenor = 0;
+	$sumaMayor = 0;
+	$bandera = 0;
+	$contador = 0;
+
+	for ($i=2; $i < 1000; $i++) //------------------Iteramos de 1 a n, donde n es el maximo valor hasta el cual queremos averiguar si hay 
+								//------------------centro numericos
+	{ 
+
+		for ($j=0; $j <= $i - 1 ; $j++) 
+		{ 
+			//echo $j,'jota<br>';
+			$sumaMenor = $sumaMenor + $j;
+		}
+
+		//echo $sumaMenor,"suma menor<br>";
+		$contador = $i + 1; //-----------------------Usamos un contador para tomar el primer valor mayor a i
+		//echo $contador,"contador<br>";
+
+
+		while ($bandera == 0) 
+		{
+
+			$sumaMayor = $sumaMayor + $contador;//
+			//echo $sumaMayor	,"suma mayor <br>";
+
+			if ($sumaMayor == $sumaMenor) //---------Si la suma de menores es igual a la de mayores, i es centro numerico
+			{
+			 	echo $i," es centro numerico.<br>";
+			 	$bandera = 1;
+			} 
+				
+			if ($sumaMayor > $sumaMenor) //----------Si la suma de los mayores es mayor a la de menores, i no es centro numerico
+			{
+				$bandera = 1;
+			}
+			
+			$contador = $contador + 1;
+			
+		}
+		
+		$sumaMenor = 0;
+		$sumaMayor =  0;
+		$bandera = 0;
+		
+	}
+	
+
+
+?>
