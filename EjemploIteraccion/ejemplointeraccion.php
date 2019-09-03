7a8a8dbfec2f19837599fa8a21f7de51b648ea0d
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-09-03 20:32:55 -0300

    Add files via upload

---
+++ EjemploIteraccion/ejemplointeraccion.php
@@ -0,0 +1,20 @@
+<?php
+	$edad;
+	$edad="15";
+	//$edad=$edad%$edad;
+	echo $edad ."<br>";
+	$i=0;
+	while ($i<$edad) 
+	{
+		echo "hola <br>";
+		$i++;
+
+	}
+	/*
+	for($i=0;$i<$edad;$i++)
+	{
+		echo "hola <br>";
+
+	}
+	*/
+?>
