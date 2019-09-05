7bf89294eef7af6d7b68ab9b3dbc17d320c2f0d7
BrunoCiccioli <54085759+BrunoCiccioli@users.noreply.github.com>
2019-09-04 21:08:12 -0300

    Add files via upload

--- Semana4/action_page.php
+++ Semana4/action_page.php
@@ -3,4 +3,11 @@ echo "Su nombre es ".$_GET['nombre'];
 echo "Su apellido es ".$_GET['apellido']; 
 $archivo=fopen('usuarios.txt', 'a');
 fwrite($archivo, $_GET['nombre']. "\n");
+fclose($archivo);
+$MiObjeto=new stdClass();
+$MiObjeto->nombre="Roberto";
+$MiObjeto->apellido="Carlos";
+
+
+var_dump($MiObjeto);
 ?>
